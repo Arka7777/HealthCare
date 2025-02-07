dcb830abf06a3d422bdf634b2f8c26e7360b7146
jyotiska222 <j.biswas0022@gmail.com>
2025-02-07 21:19:36 +0530

    add: Contact Us page Added with Formspree

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,18 @@ import { createBrowserRouter, RouterProvider, Navigate, useNavigate } from "reac
 import Navbar from "./Home_components/Navbar"
 import Hero from "./Home_components/HeroSection/Hero"
 import Feature from "./Home_components/Feature"
+import ContactUs from "./Home_components/Contact_us"
 
 
 
 
 export default function App() {
-  
-  
 
-  
 
-  
+
+
+
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -24,7 +25,16 @@ export default function App() {
         </>
       ),
     },
-    
+    {
+      path: "/contact_us",
+      element: (
+        <>
+          <Navbar />
+          <ContactUs />
+        </>
+      ),
+    },
+
   ])
 
   return <RouterProvider router={router} />
